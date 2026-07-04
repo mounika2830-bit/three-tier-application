032df680266ba3e72189ad07f47a0ca9e3b25e8a
Reddy Mounica <mounika2830@gmail.com>
2026-07-04 09:30:32 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'db-instance1.c1y0qscqu9xl.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678',
+    DB_DATABASE : 'webappdb'
+});
