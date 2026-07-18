4332109e7a057178fd748c05c3b733f9f1a5733d
Reddy Mounica <mounika2830@gmail.com>
2026-07-18 14:55:20 +0530

    Update database host in DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'db-instance1.c1y0qscqu9xl.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.c1y0qscqu9xl.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : '12345678',
     DB_DATABASE : 'webappdb'
